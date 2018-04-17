17b1fb38107e5eff03e7742efafe560189c96cf4
Danny Thanakrit Boonquarmdee <pb.danny@gmail.com>
2018-04-17 11:13:02 +0700

    test script at office

--- MSAccess_to_csv_windows.py
+++ MSAccess_to_csv_windows.py
@@ -12,7 +12,7 @@ Look for MSAccess driver install in windows
 # list all driver available
 drivers = [x for x in pyodbc.drivers() if x.startswith('Microsoft Access Driver')]
 
-# select best driver param, 1st 'ACE', 2nd 'Jet' 
+# select best driver param, 1st 'ACE', 2nd 'Jet'
 for driver in drivers:
     if driver == 'Microsoft Access Driver (*.mdb, *.accdb)':
         driver_param = '{Microsoft Access Driver (*.mdb, *.accdb)}'
@@ -28,36 +28,38 @@ dict_driver_param = {'{}':'Need to install MS Access driver.',
 print('Found driver : ' + driver_param + '\n' + dict_driver_param[driver_param])
 
 # create connection string
-DB = 'E:\\OSS DataSales 201802.mdb'
+DB = 'C:\\Users\\Thanakrit.B\\Downloads\\OSS DataSales 201802.mdb'
 conn_str = (
     r'DRIVER=' + driver_param + ';'
     r'DBQ='+ DB +';'
     )
 # Initate Connection
-cnxn = pyodbc.connect(conn_str)
-# Instantiate Curser
-crsr = cnxn.cursor()
-# list all tables
-tables = [x.table_name for x in crsr.tables(tableType='TABLE')]
-print('List of all tables in DB')
-print(tables)
-
-# Create temp csv directory for store files
+con = pyodbc.connect(conn_str)
 try:
-    os.mkdir('../temp_csv')
-except:
-    print('temp_csv dir created')
+    with con.cursor() as cur:
+        # list all tables
+        tables = [x.table_name for x in cur.tables(tableType='TABLE')]
+        print('List of all tables in DB')
+        print(tables)
 
-# Export each table to temp_csv
-os.chdir('../temp_csv')
+    # Create temp csv directory for store files
+    try:
+        os.mkdir('..\\temp_csv')
+    except:
+        print('temp_csv dir created')
 
-for table in tables[3:4]:
-    if table != '':
-        filename = table.replace(" ","_") + ".csv"
-        print("Dumping " + table)
-        rows = crsr.execute("SELECT * FROM " + table)
-        with open(filename, 'w', encoding = 'utf-8') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow([x[0] for x in crsr.description])
-            for row in rows:
-                writer.writerow(row)
+    # Export each table to temp_csv
+    os.chdir('..\\temp_csv')
+
+    for table in tables[2:]:
+        if table != '':
+            filename = table.replace(" ","_") + ".csv"
+            print("Dumping " + table)
+            rows = cur.execute("SELECT * FROM " + table)
+            with open(filename, 'w', encoding = 'utf-8') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow([x[0] for x in cur.description])
+                for row in rows:
+                    writer.writerow(row)
+finally:
+    con.close()
